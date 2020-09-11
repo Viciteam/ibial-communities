3085f7e5891e60b883cff8a44258361e403548e1
Kenneth Arphel Balboa <e3arphie@gmail.com>
2020-09-12 00:15:33 +0800

    Inter MS Communications / Bearer Tokens

--- routes/api.php
+++ routes/api.php
@@ -30,56 +30,60 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     Route::post("update/community-cover-photo", 'CommunityController@updateCommunityCoverPhoto');
 
 });*/
-Route::group([
-    'prefix' => 'business',
-], function () {
-    Route::post("/add", 'BusinessController@createBusiness');
-    Route::get("/{id}", 'BusinessController@getBusinessDetails');
-    Route::put("update/{id}/business-privacy", 'BusinessController@updateBusinessPrivacy');
-    Route::put("update/{id}/business-views", 'BusinessController@updateBusinessViews');
-    Route::put("update/{id}/business-profile", 'BusinessController@updateBusinessProfile');
-    Route::post("update/business-profile-pic", 'BusinessController@updateBusinessProfilePic');
-    Route::post("update/business-cover-photo", 'BusinessController@updateBusinessCoverPhoto');
-    Route::post("/add-business-partner", 'BusinessController@addBusinessPartner');
-    Route::post("/accept-bussiness-partner-request", 'BusinessController@acceptBusinessPartnerRequest');
-    Route::get("/get-partners/{id}", 'BusinessController@getPartners');
-});
+
+Route::group(['middleware' => 'checktoken'], function(){
+
+    Route::group([
+        'prefix' => 'business',
+    ], function () {
+        Route::post("/add", 'BusinessController@createBusiness');
+        Route::get("/{id}", 'BusinessController@getBusinessDetails');
+        Route::put("update/{id}/business-privacy", 'BusinessController@updateBusinessPrivacy');
+        Route::put("update/{id}/business-views", 'BusinessController@updateBusinessViews');
+        Route::put("update/{id}/business-profile", 'BusinessController@updateBusinessProfile');
+        Route::post("update/business-profile-pic", 'BusinessController@updateBusinessProfilePic');
+        Route::post("update/business-cover-photo", 'BusinessController@updateBusinessCoverPhoto');
+        Route::post("/add-business-partner", 'BusinessController@addBusinessPartner');
+        Route::post("/accept-bussiness-partner-request", 'BusinessController@acceptBusinessPartnerRequest');
+        Route::get("/get-partners/{id}", 'BusinessController@getPartners');
+    });
 
 
-// update from Arphie
+    // update from Arphie
 
-Route::group([
-    'prefix' => 'company',
-], function () {
-    Route::post("add", 'CompanyController@insert'); 
-});
+    Route::group([
+        'prefix' => 'company',
+    ], function () {
+        Route::post("add", 'CompanyController@insert'); 
+    });
 
-Route::group([
-    'prefix' => 'teams',
-], function () {
-    Route::post("add", 'CompanyController@addTeam'); 
-    Route::post("invite", 'CompanyController@invite'); 
-    Route::post("uninvite", 'CompanyController@uninvite'); 
+    Route::group([
+        'prefix' => 'teams',
+    ], function () {
+        Route::post("add", 'CompanyController@addTeam'); 
+        Route::post("invite", 'CompanyController@invite'); 
+        Route::post("uninvite", 'CompanyController@uninvite'); 
 
-    Route::get("members", 'CompanyController@members'); 
-});
+        Route::get("members", 'CompanyController@members'); 
+    });
 
-Route::group([
-    'prefix' => 'kb',
-], function () {
-    Route::post("insert", 'KnowledgeBaseController@insert'); 
-    Route::post("edit", 'KnowledgeBaseController@edit'); 
-    Route::post("deactive", 'KnowledgeBaseController@deactive'); 
-});
+    Route::group([
+        'prefix' => 'kb',
+    ], function () {
+        Route::post("insert", 'KnowledgeBaseController@insert'); 
+        Route::post("edit", 'KnowledgeBaseController@edit'); 
+        Route::post("deactive", 'KnowledgeBaseController@deactive'); 
+    });
 
-Route::group([
-    'prefix' => 'manage',
-], function () {
-    Route::post("/", 'CompanyController@manage'); 
-});
+    Route::group([
+        'prefix' => 'manage',
+    ], function () {
+        Route::post("/", 'CompanyController@manage'); 
+    });
 
-Route::group([
-    'prefix' => 'hashtags',
-], function () {
-    Route::get("suggest", 'CompanyController@suggest');
+    Route::group([
+        'prefix' => 'hashtags',
+    ], function () {
+        Route::get("suggest", 'CompanyController@suggest');
+    });
 });
